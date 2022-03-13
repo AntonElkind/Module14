545c3384154cc91d3a0abac4ab19a54405486c76
Anton Elkind <anton.elkind@gmail.com>
2022-03-13 21:03:21 +0200

    Add main program

--- Mod14/Mod14/main.cpp
+++ Mod14/Mod14/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include "trie.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "HW Module 12" << endl;
+
+	Trie* trie = new Trie;
+
+	trie->insertKey("mil");
+	trie->insertKey("milk");
+	trie->insertKey("milkyway");
+	trie->insertKey("macro");
+	trie->insertKey("skill");
+	trie->insertKey("skillfactory");
+	trie->insertKey("sky");
+
+	std::string userIn;
+	bool run = true;
+	while (run)
+	{
+		cout << "Enter prefix or \"exit\": ";
+		cin >> userIn;
+		if (userIn.compare("exit") == 0)
+		{
+			run = false;
+			break;
+		}
+		if (trie->searchKey(userIn, true) == false)
+		{
+			cout << "No variants" << endl;;
+		}
+		cout << endl;
+	}
+	delete trie;
 
 	return 0;
 }
